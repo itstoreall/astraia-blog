670d16232852302e0193e6849f16530edbb8ef1d
Serhii Stanislav <serhiistanislav@gmail.com>
2023-08-04 00:35:16 +0300

    ADD | logo styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NextThemeProvider from "../providers/themeProvider";
 import "@/styles/global.scss";
+import MainContainer from "@/components/Containers/MainContainer";
+import ArticlesButton from "@/components/ArticlesButton";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import MainContainer from "@/components/Containers/MainContainer";
-// import ArticlesButton from "@/components/ArticlesButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,9 +26,9 @@ export default function RootLayout({
           <div className="globalWrapper">
             <Header />
             <main className="main">
+              <ArticlesButton />
               <MainContainer>{children}</MainContainer>
             </main>
-            {/* <ArticlesButton /> */}
             <Footer />
           </div>
         </NextThemeProvider>
--- components/ArticlesButton/ArticlesButton.tsx
+++ components/ArticlesButton/ArticlesButton.tsx
@@ -1,38 +1,48 @@
 "use client";
 import { useTheme } from "next-themes";
+import { usePathname } from "next/navigation"; // useRouter, useSearchParams
 import Link from "next/link";
-// import { THEME_BY_DEFAULT } from "@/constants";
 import { getCurrentTheme } from "@/utils";
 import s from "./ArticlesButton.module.scss";
-
-// const defaultTheme = THEME_BY_DEFAULT;
 
 const ArticlesButton = () => {
   const { theme } = useTheme();
 
-  const width = "29";
-  const height = "29";
+  const pathname = usePathname();
+
+  console.log("pathname", pathname);
+
+  const desc = {
+    width: "29",
+    height: "29",
+    viewBox: "0 0 32 32",
+  };
+
+  const width = "25";
+  const height = "25";
   const viewBox = "0 0 32 32";
 
   const currentTheme = getCurrentTheme(theme);
-  // const currentTheme =
-  //   theme === "light" || theme === "dark" ? theme : defaultTheme;
 
   return (
-    <nav className={`${s.articlesButtonWrap} ${s[currentTheme]}`}>
-      <Link href={"/articles"}>
-        <div className={s.articlesButton}>
-          <svg
-            width={width}
-            height={height}
-            viewBox={viewBox}
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M0 0h8v8H0zm12 2h20v4H12zM0 12h8v8H0zm12 2h20v4H12zM0 24h8v8H0zm12 2h20v4H12z" />
-          </svg>
-        </div>
-      </Link>
-    </nav>
+    <>
+      {pathname !== "/articles" && (
+        <nav className={`${s.articlesButtonWrap} ${s[currentTheme]}`}>
+          <Link href={"/articles"}>
+            <div className={s.articlesButton}>
+              <svg
+                width={width}
+                height={height}
+                viewBox={viewBox}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 0h8v8H0zm12 2h20v4H12zM0 12h8v8H0zm12 2h20v4H12zM0 24h8v8H0zm12 2h20v4H12z" />
+              </svg>
+            </div>
+          </Link>
+        </nav>
+      )}
+    </>
   );
 };
 
