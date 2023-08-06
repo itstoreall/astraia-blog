8ceed874d505acb14690700314d7aa0e1ca61f9f
Serhii Stanislav <serhiistanislav@gmail.com>
2023-08-06 12:42:59 +0300

    ADD | landscape

--- components/Image/ImageHandler.tsx
+++ components/Image/ImageHandler.tsx
@@ -4,6 +4,7 @@ import useViewport from "@/hooks/useViewport";
 import useProportion from "@/hooks/useProportion";
 import { WEB3_STORAGE } from "@/constants";
 import defaultImage from "@/assets/images/defaultImage.jpg";
+// import { useState } from "react";
 // import { MOBILE } from "@/styles/vars";
 
 export interface IImageHandlerProps {
@@ -18,9 +19,12 @@ const setImageSrc = (cid: string) =>
   cid ? `https://${cid}.${ipfs}` : defaultImage;
 
 const ImageHandler = ({ cid, alt, size }: IImageHandlerProps) => {
+  // const [w, setW] = useState<number>();
+  // const [h, setH] = useState<number>();
+
   const isFull = () => size === "full";
 
-  const { viewport, landscape } = useViewport();
+  const { viewport } = useViewport();
   const { width, height } = useProportion(
     900,
     450,
@@ -35,11 +39,13 @@ const ImageHandler = ({ cid, alt, size }: IImageHandlerProps) => {
       : 900
   );
 
+  // useEffect(() => {}, [width, height, landscape]);
+
   // console.log("landscape", landscape);
 
   return (
     <>
-      {viewport && landscape && (
+      {viewport && (
         <Image
           src={setImageSrc(cid)}
           unoptimized
