12c12a5bf945f94a6e7054fd07b04acd370ac8fb
Serhii Stanislav <serhiistanislav@gmail.com>
2023-10-05 00:33:00 +0300

    FIX | getCurrentTheme to theme

--- components/ArticlesButton/ArticlesButton.tsx
+++ components/ArticlesButton/ArticlesButton.tsx
@@ -2,7 +2,7 @@
 import { useTheme } from "next-themes";
 import { usePathname } from "next/navigation"; // useRouter, useSearchParams
 import Link from "next/link";
-import { getCurrentTheme } from "@/utils";
+// import { getCurrentTheme } from "@/utils";
 import s from "./ArticlesButton.module.scss";
 import FixedContainer from "../Containers/FixedContainer";
 
@@ -21,26 +21,28 @@ const ArticlesButton = () => {
   const height = "25";
   const viewBox = "0 0 32 32";
 
-  const currentTheme = getCurrentTheme(theme);
+  // const currentTheme = getCurrentTheme(theme);
 
   return (
     <>
       {pathname !== "/articles" && (
         <FixedContainer>
-          <nav className={`${s.articlesButtonWrap} ${s[currentTheme]}`}>
-            <Link href={"/articles"}>
-              <div className={s.articlesButton}>
-                <svg
-                  width={width}
-                  height={height}
-                  viewBox={viewBox}
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M0 0h8v8H0zm12 2h20v4H12zM0 12h8v8H0zm12 2h20v4H12zM0 24h8v8H0zm12 2h20v4H12z" />
-                </svg>
-              </div>
-            </Link>
-          </nav>
+          {theme && (
+            <nav className={`${s.articlesButtonWrap} ${s[theme]}`}>
+              <Link href={"/articles"}>
+                <div className={s.articlesButton}>
+                  <svg
+                    width={width}
+                    height={height}
+                    viewBox={viewBox}
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M0 0h8v8H0zm12 2h20v4H12zM0 12h8v8H0zm12 2h20v4H12zM0 24h8v8H0zm12 2h20v4H12z" />
+                  </svg>
+                </div>
+              </Link>
+            </nav>
+          )}
         </FixedContainer>
       )}
     </>
